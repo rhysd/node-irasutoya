fcff8cef28f282a8cec8daf7ab2b41fb811cab6b
rhysd <lin90162@yahoo.co.jp>
2015-12-02 00:26:56 +0900

    add type definitions

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+import Promise = require('bluebird');
+export declare type CategoryName = string;
+export interface Category {
+    title: CategoryName;
+    url: string;
+}
+export interface Irasuto {
+    name: string;
+    image_url: string;
+    detail_url: string;
+}
+export declare type Irasutoya = Map<CategoryName, Irasuto[]>;
+export declare function fetchCategories(): Promise<Category[]>;
+export declare function fetchIrasutoOf(category: Category): Promise<Irasuto[]>;
+export declare function fetchAllIrasuto(): Promise<Irasutoya>;
